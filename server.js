8c3288a9eb9897c44c90a4ee6a1dcd1684da633b
Stiven C <08.cacais.willian.803@gmail.com>
2026-09-10 17:41:04 -0500

    corregir server.js

--- server.js
+++ server.js
@@ -8,10 +8,9 @@ const BaseDatos = require("./src/db/BaseDatos");
 
 const rutasPreguntas = require("./src/routes/preguntas");
 const rutasContextos = require("./src/routes/contextos");
+
 const rutasUsuarios = require("./src/routes/usuarios");
 const rutasIntentos = require("./src/routes/intentos");
-const rutasImagenes = require("./src/routes/imagenes");
-const rutasAreas = require("./src/routes/areas");
 
 const app = express();
 
@@ -22,9 +21,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(
-    express.static(
-        path.join(__dirname, "public")
-    )
+express.static(
+path.join(__dirname, "public")
+)
 );
 
 // =========================
@@ -32,8 +31,8 @@ app.use(
 // =========================
 
 app.use(
-    "/api/preguntas",
-    rutasPreguntas(db)
+"/api/preguntas",
+rutasPreguntas(db)
 );
 
 // =========================
@@ -41,8 +40,8 @@ app.use(
 // =========================
 
 app.use(
-    "/api/contextos",
-    rutasContextos(db)
+"/api/contextos",
+rutasContextos(db)
 );
 
 // =========================
@@ -50,8 +49,8 @@ app.use(
 // =========================
 
 app.use(
-    "/api",
-    rutasUsuarios(db)
+"/api",
+rutasUsuarios(db)
 );
 
 // =========================
@@ -59,26 +58,8 @@ app.use(
 // =========================
 
 app.use(
-    "/api",
-    rutasIntentos(db)
-);
-
-// =========================
-// RUTAS DE IMAGENES
-// =========================
-
-app.use(
-    "/api",
-    rutasImagenes()
-);
-
-// =========================
-// RUTAS DE AREAS
-// =========================
-
-app.use(
-    "/api/areas",
-    rutasAreas
+"/api",
+rutasIntentos(db)
 );
 
 // =========================
@@ -86,7 +67,11 @@ app.use(
 // =========================
 
 app.listen(3000, () => {
-    console.log(
-        "🚀 Servidor corriendo en http://localhost:3000"
-    );
+
+```
+console.log(
+    "🚀 Servidor corriendo en http://localhost:3000"
+);
+```
+
 });
